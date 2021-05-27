46e4c05d591a7b4e829bb8ec35a15b932b70dafb
EC2 Default User <ec2-user@ip-172-31-74-41.ec2.internal>
2021-05-27 07:12:02 +0000

    auth, storage and preds

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from './Button'
+import Posts from './Posts';
+import Post from './Post';
+import Header from './Header';
+import CreatePost from './CreatePost';
+
+export { Button, Posts, Post, Header, CreatePost }
